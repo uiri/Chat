c40f2ceae6ed36be2fbcf5b10935de90581b80fb
Uiri Noyb <uiri00@gmail.com>
2011-05-06 19:27:58 -0400

    need to successfully combine server and client and implement a method for quitting...

--- client.c
+++ client.c
@@ -10,43 +10,41 @@
 
 int main(void) {
 
-	struct sockaddr_storage client_addr;
-	socklen_t addr_size;
-	struct addrinfo hints, *res;
-	int sock, client_sock, quit;
-	char first;
+  struct sockaddr_storage client_addr;
+  socklen_t addr_size;
+  struct addrinfo hints, *res;
+  int sock, client_sock, quit, recvstat;
+  int buffer_size = 250;
+  
+  char *buffer;
+  buffer = malloc(256 * sizeof(char));
+  char *recv_buffer;
+  recv_buffer = malloc(256 * sizeof(char));
+  
+  printf("First message:");
+  fgets(buffer, 250, stdin);
 
-	char *buffer;
-	buffer = malloc(256 * sizeof(char));
-	char *recv_buffer;
-	recv_buffer = malloc(256 * sizeof(char));
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  getaddrinfo("127.0.0.1", PORT, &hints, &res);
+  
+  sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+  connect(sock, res->ai_addr, res->ai_addrlen);
+  
+  while (quit != 1) {
+    send(sock, buffer, 256, 0);
+    recv(sock, recv_buffer, 256, 0);
+    printf("%sSay:", recv_buffer);
+    fgets(buffer, 250, stdin);
+    if (buffer[0] == "/") {
+      quit=1;
+    }
+  }
 
-	scanf("%s\0", buffer);
-
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	getaddrinfo("127.0.0.1", PORT, &hints, &res);
-	
-	sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-	connect(sock, res->ai_addr, res->ai_addrlen);
-
-	first = *buffer;
-	quit = (int)first;
-	
-	while (quit != 1) {
-	  printf("send 1 to quit");
-	  send(sock, buffer, 256, 0);
-	  scanf("%s\0", buffer);
-	  first = *buffer;
-	  quit = (int)first;
-	  recv(sock, recv_buffer, 256, 0);
-	  //printf("Recieved echo: %s\n",recv_buffer);
-	}
-
-	freeaddrinfo(res);
-	close(sock);
-	free(buffer);
-	free(recv_buffer);
-	return 0;
+  freeaddrinfo(res);
+  close(sock);
+  free(buffer);
+  free(recv_buffer);
+  return 0;
 }
--- server.c
+++ server.c
@@ -10,48 +10,41 @@
 
 int main(void) {
 	
-	struct sockaddr_storage client_addr;
-	socklen_t addr_size;
-	struct addrinfo hints, *res;
-	int sock, client_sock, quit;
-	char first;
+  struct sockaddr_storage client_addr;
+  socklen_t addr_size;
+  struct addrinfo hints, *res;
+  int sock, client_sock, quit, recvstat;
+  int buffer_size = 250;
+  
+  char *recv_buffer;
+  recv_buffer = malloc(256 * sizeof(char));
+  char *buffer;
+  buffer = malloc(256 * sizeof(char));
+  
+  printf("First message:");
+  fgets(buffer, 250, stdin);
 
-	char *recv_buffer;
-	recv_buffer = malloc(256 * sizeof(char));
-	char *buffer;
-	buffer = malloc(256 * sizeof(char));
-
-	scanf("%s\0", buffer);
-
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;  // use IPv4 or IPv6, whichever
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
-
-	getaddrinfo("127.0.0.1", PORT, &hints, &res);
-	
-	sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-	bind(sock,res->ai_addr,res->ai_addrlen);
-	listen(sock, 10);
-	
-	client_sock = accept(sock,(struct sockaddr *)&client_addr, &addr_size);
-	
-	first = *buffer;
-	quit = (int)first;
-
-	while (quit != 1) {
-	  printf("send 1 to quit");
-	  recv(client_sock, recv_buffer, 256, 0);
-	  //printf("Recieved: %s\nEchoing to client\n",recv_buffer);
-	  send(client_sock, buffer, 256, 0);
-	  scanf("%s\0", buffer);
-	  first = *buffer;
-	  quit = (int)first;
-	}
-
-	freeaddrinfo(res);
-	close(sock); 
-	free(recv_buffer);
-	free(buffer);
-	return 0;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+  getaddrinfo("127.0.0.1", PORT, &hints, &res);
+  
+  sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+  bind(sock,res->ai_addr,res->ai_addrlen);
+  listen(sock, 10);
+  client_sock = accept(sock,(struct sockaddr *)&client_addr, &addr_size);
+  
+  while (quit != 1) {
+    send(client_sock, buffer, 256, 0);
+    recv(client_sock, recv_buffer, 256, 0);
+    printf("%sSay:", recv_buffer);
+    fgets(buffer, 250, stdin);
+  }
+  
+  freeaddrinfo(res);
+  close(sock); 
+  free(recv_buffer);
+  free(buffer);
+  return 0;
 }
